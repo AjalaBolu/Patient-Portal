dd54d604ffb2794a55fa90bda6ea944e5c01b5d1
BOLUAJALA <136012260+BOLUAJALA@users.noreply.github.com>
2026-03-26 12:02:13 +0100

    Auto create superuser

--- Portal/views.py
+++ Portal/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        username='admin',
-        email='admin@email.com',
-        password='admin123'
-    )
--- patients/apps.py
+++ patients/apps.py
@@ -1,6 +1,17 @@
-from django.apps import AppConfig
 
+
+from django.apps import AppConfig
 
 class PatientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'patients'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@email.com',
+                password='admin123'
+            )
