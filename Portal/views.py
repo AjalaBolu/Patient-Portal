15d9394faebe0035e83b7a9569c1d5f9c8dabdd6
BOLUAJALA <136012260+BOLUAJALA@users.noreply.github.com>
2026-03-26 11:47:13 +0100

    superuser

--- Portal/views.py
+++ Portal/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@email.com',
+        password='admin123'
+    )
